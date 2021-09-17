ad663f7236348e3e81e8d50d411c087c97d99b43
Kent D <kent.daleng@nav.no>
2021-09-17 22:12:37 +0200

    make ginuudan inert for now

--- ginuudan/__main__.py
+++ ginuudan/__main__.py
@@ -18,33 +18,36 @@ def configure(settings: kopf.OperatorSettings, **_):
     settings.posting.enabled = False
 
 
-@kopf.on.field(
-    "pods",
-    annotations={"ginuudan.nais.io/dwindle": "true"},
-    field="status.containerStatuses",
-)
-def status_change(logger, **kwargs):
-    pod = kube.Pod(core_v1, event, logger=logger, **kwargs)
-    if pod.app.name == "":
-        event.error(pod, f"Required field `labels.app` is not set")
-        return
-    if not pod.app.terminated:
-        return
+# Ginuudan is inert,
+# TODO: have a closer look on Monday
 
-    for sidecar in pod.running_sidecars():
-        if sidecar not in actions:
-            logger.error(f"I don't know how to shut down {sidecar}")
-            event.error(pod, f"I don't know how to shut down {sidecar}")
-            continue
-        logger.info(f"Shutting down {sidecar}")
-        event.normal(pod, f"Shutting down {sidecar}")
-        action = actions[sidecar]
-        if action["type"] == "exec":
-            pod.exec_command(sidecar, action["command"].split())
-        elif action["type"] == "portforward":
-            pod.port_forward(sidecar, action["method"], action["path"], action["port"])
-        else:
-            logger.error(f"Unknown action.type `{action['type']}`")
-        metrics.sidecars_shutdown_total.labels(
-            sidecar, pod.app.name, pod.namespace
-        ).inc()
+# @kopf.on.field(
+#     "pods",
+#     annotations={"ginuudan.nais.io/dwindle": "true"},
+#     field="status.containerStatuses",
+# )
+# def status_change(logger, **kwargs):
+#     pod = kube.Pod(core_v1, event, logger=logger, **kwargs)
+#     if pod.app.name == "":
+#         event.error(pod, f"Required field `labels.app` is not set")
+#         return
+#     if not pod.app.terminated:
+#         return
+
+#     for sidecar in pod.running_sidecars():
+#         if sidecar not in actions:
+#             logger.error(f"I don't know how to shut down {sidecar}")
+#             event.error(pod, f"I don't know how to shut down {sidecar}")
+#             continue
+#         logger.info(f"Shutting down {sidecar}")
+#         event.normal(pod, f"Shutting down {sidecar}")
+#         action = actions[sidecar]
+#         if action["type"] == "exec":
+#             pod.exec_command(sidecar, action["command"].split())
+#         elif action["type"] == "portforward":
+#             pod.port_forward(sidecar, action["method"], action["path"], action["port"])
+#         else:
+#             logger.error(f"Unknown action.type `{action['type']}`")
+#         metrics.sidecars_shutdown_total.labels(
+#             sidecar, pod.app.name, pod.namespace
+#         ).inc()
