59b0eae7321d050caa69ce137426444b4d444227
Kent D <kent.daleng@nav.no>
2021-09-18 09:04:30 +0200

    use kopf.on.event
    NOTE: this is basically nuclear mode. ginuudan does not send pod events,
    and it more or less knocks on every door until it thinks things are fine.
    
    i think it's easiest to revert back to the original logic once the
    state is somewhat clean again

--- ginuudan/__main__.py
+++ ginuudan/__main__.py
@@ -10,7 +10,7 @@ actions = actions.load_sidecar_actions(basepath / "actions.yml")
 
 core_v1 = kube.init_corev1()
 metrics = prometheus.Metrics()
-event = kube.Event(core_v1)
+pod_event = kube.Event(core_v1)
 
 
 @kopf.on.startup()
@@ -18,36 +18,32 @@ def configure(settings: kopf.OperatorSettings, **_):
     settings.posting.enabled = False
 
 
-# Ginuudan is inert,
-# TODO: have a closer look on Monday
+@kopf.on.event(
+    "pods",
+    annotations={"ginuudan.nais.io/dwindle": "true"},
+)
+def status_change(event, logger, **kwargs):
+    pod = kube.Pod(core_v1, pod_event, event, logger=logger, **kwargs)
+    if pod.app.name == "":
+        pod_event.error(pod, f"Required field `labels.app` is not set")
+        return
+    if not pod.app.terminated:
+        return
 
-# @kopf.on.field(
-#     "pods",
-#     annotations={"ginuudan.nais.io/dwindle": "true"},
-#     field="status.containerStatuses",
-# )
-# def status_change(logger, **kwargs):
-#     pod = kube.Pod(core_v1, event, logger=logger, **kwargs)
-#     if pod.app.name == "":
-#         event.error(pod, f"Required field `labels.app` is not set")
-#         return
-#     if not pod.app.terminated:
-#         return
-
-#     for sidecar in pod.running_sidecars():
-#         if sidecar not in actions:
-#             logger.error(f"I don't know how to shut down {sidecar}")
-#             event.error(pod, f"I don't know how to shut down {sidecar}")
-#             continue
-#         logger.info(f"Shutting down {sidecar}")
-#         event.normal(pod, f"Shutting down {sidecar}")
-#         action = actions[sidecar]
-#         if action["type"] == "exec":
-#             pod.exec_command(sidecar, action["command"].split())
-#         elif action["type"] == "portforward":
-#             pod.port_forward(sidecar, action["method"], action["path"], action["port"])
-#         else:
-#             logger.error(f"Unknown action.type `{action['type']}`")
-#         metrics.sidecars_shutdown_total.labels(
-#             sidecar, pod.app.name, pod.namespace
-#         ).inc()
+    for sidecar in pod.running_sidecars():
+        if sidecar not in actions:
+            logger.error(f"I don't know how to shut down {sidecar}")
+            pod_event.error(pod, f"I don't know how to shut down {sidecar}")
+            continue
+        logger.info(f"Shutting down {sidecar}")
+        pod_event.normal(pod, f"Shutting down {sidecar}")
+        action = actions[sidecar]
+        if action["type"] == "exec":
+            pod.exec_command(sidecar, action["command"].split())
+        elif action["type"] == "portforward":
+            pod.port_forward(sidecar, action["method"], action["path"], action["port"])
+        else:
+            logger.error(f"Unknown action.type `{action['type']}`")
+        metrics.sidecars_shutdown_total.labels(
+            sidecar, pod.app.name, pod.namespace
+        ).inc()
